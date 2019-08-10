c8fcf03bf8fa6db19e8aa5a07644ecdefce28fae
Keita Watanabe <Nal3eCh@gmail.com>
2019-08-10 17:04:35 +0900

    add container views

---
+++ SwiftUICatalog/ViewLayoutAndPresentation /ContainerViews/GroupBoxView.swift
@@ -0,0 +1,23 @@
+//
+//  GroupBoxView.swift
+//  SwiftUICatalog
+//
+//  Created by Keita Watanabe on 2019/08/10.
+//  Copyright © 2019 usagi-tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct GroupBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupBoxView()
+    }
+}
+#endif
