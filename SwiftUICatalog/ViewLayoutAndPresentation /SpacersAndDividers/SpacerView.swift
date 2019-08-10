1a39782ac8806a938a0c939b2e51df2694f34de3
Keita Watanabe <Nal3eCh@gmail.com>
2019-08-10 17:11:11 +0900

    add spacers and dividers

---
+++ SwiftUICatalog/ViewLayoutAndPresentation /SpacersAndDividers/SpacerView.swift
@@ -0,0 +1,23 @@
+//
+//  SpacerView.swift
+//  SwiftUICatalog
+//
+//  Created by Keita Watanabe on 2019/08/10.
+//  Copyright © 2019 usagi-tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpacerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SpacerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacerView()
+    }
+}
+#endif
