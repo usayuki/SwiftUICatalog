7e31913f5ac2a23eb5f8aea8c0f1f7635559ce1c
Keita Watanabe <Nal3eCh@gmail.com>
2019-08-10 13:41:39 +0900

    add value selectors

---
+++ SwiftUICatalog/ViewsAndControls/Value Selectors/SliderView.swift
@@ -0,0 +1,23 @@
+//
+//  SliderView.swift
+//  SwiftUICatalog
+//
+//  Created by Keita Watanabe on 2019/08/10.
+//  Copyright © 2019 usagi-tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView()
+    }
+}
+#endif
