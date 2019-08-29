c97dfefce4e2484270f83ebb7f6b29a67da0270f
usayuki <masapp.01a@gmail.com>
2019-08-29 09:03:35 +0900

    fix model

--- SwiftUICatalog/ChatUI/ChatModel.swift
+++ SwiftUICatalog/ChatUI/ChatModel.swift
@@ -15,10 +15,22 @@ struct ChatModel: Identifiable {
 }
 
 let chatModels: [ChatModel] = [
-    .init(id: 1, message: "短文", isOutgoing: false),
-    .init(id: 2, message: "短文", isOutgoing: true),
-    .init(id: 3, message: "少し長めの文章", isOutgoing: false),
-    .init(id: 4, message: "少し長めの文章\n改行すると吹き出しも伸びる", isOutgoing: false),
-    .init(id: 5, message: "少し長めの文章", isOutgoing: true),
-    .init(id: 6, message: "少し長めの文章\n改行すると\nその分だけ吹き出しも伸びる", isOutgoing: true)
+    .init(id: 1, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 2, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 3, message: "I am usayuki.\nHello World!", isOutgoing: false),
+    .init(id: 4, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 5, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 6, message: "I am _nabech.\nHello World!", isOutgoing: true),
+    .init(id: 7, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 8, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 9, message: "I am usayuki.\nHello World!", isOutgoing: false),
+    .init(id: 10, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 11, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 12, message: "I am _nabech.\nHello World!", isOutgoing: true),
+    .init(id: 13, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 14, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 15, message: "I am usayuki.\nHello World!", isOutgoing: false),
+    .init(id: 16, message: "I am usayuki.", isOutgoing: false),
+    .init(id: 17, message: "I am _nabech.", isOutgoing: true),
+    .init(id: 18, message: "I am _nabech.\nHello World!", isOutgoing: true),
 ]
